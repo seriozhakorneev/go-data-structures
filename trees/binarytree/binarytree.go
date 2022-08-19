a617d95e297503dc13a549f62ae5e80952031b8b
korneevsergey <seriozhakorneev@gmail.com>
2022-08-19 19:06:39 +0500

    trees
    Binary tree minor fixes.

--- trees/binarytree/binarytree.go
+++ trees/binarytree/binarytree.go
@@ -1,7 +1,4 @@
-package main
-
-// TODO: !!!!!!!
-//package binarytree
+package binarytree
 
 // Tree represents a binary tree
 // that holds values of any type.
@@ -37,9 +34,9 @@ func GenerateFromRange(from, to int) Tree[int] {
 	f, s := a[:m], a[m+1:]
 
 	if len(f) > len(s) {
-		t.Depth = calcDepth(f)
+		t.Depth = CalcDepth(f)
 	}
-	t.Depth = calcDepth(s)
+	t.Depth = CalcDepth(s)
 
 	t.Root.Left = generateNode(f, &t.Len)
 	t.Root.Right = generateNode(s, &t.Len)
@@ -54,9 +51,9 @@ func GenerateFromSlice(a []int) Tree[int] {
 	f, s := a[:m], a[m+1:]
 
 	if len(f) > len(s) {
-		t.Depth = calcDepth(f)
+		t.Depth = CalcDepth(f)
 	}
-	t.Depth = calcDepth(s)
+	t.Depth = CalcDepth(s)
 
 	t.Root.Left = generateNode(f, &t.Len)
 	t.Root.Right = generateNode(s, &t.Len)
@@ -88,8 +85,9 @@ func generateSlice(from, to int) []int {
 	return a
 }
 
-//TODO maybe incorrect
-func calcDepth(a []int) int {
+// TODO maybe incorrect
+
+func CalcDepth(a []int) int {
 	if len(a) > 2 {
 		depth := float64(len(a)) / float64(2)
 		if depth/10 != 0 {
@@ -104,58 +102,3 @@ func calcDepth(a []int) int {
 // нужен при добавлении к дереву элемента
 // оптимизация узлов дерева для наиболее
 // оптимальной расстановки
-
-// TODO print tree func (not sure bout correct Depth calc)
-// на основе обхода дерева
-
-//func (t *Tree[T]) PrintAll() {
-//
-//	fmt.Printf("\nLength:%d Depth:%d Root Node:%v\n", t.Len, t.Depth, t.Root.Data)
-//
-//	var printRec func(level int, node *Node[T])
-//	printRec = func(level int, node *Node[T]) {
-//
-//		if node == nil {
-//			return
-//		}
-//		fmt.Print(" ", node.Data)
-//		printRec(level+1, node.Right)
-//		printRec(level+1, node.Left)
-//	}
-//
-//	if t.Depth > 0 {
-//		printRec(1, t.Root.Right)
-//		printRec(1, t.Root.Left)
-//	}
-//}
-
-func main() {
-	// creating new binary tree with range from 1 to 10
-	//t := NewTree("F")
-	//
-	//t.Root.Left = &Node[string]{Data: "B"}
-	//t.Root.Left.Left = &Node[string]{Data: "A"}
-	//t.Root.Left.Right = &Node[string]{Data: "D"}
-	//t.Root.Left.Right.Left = &Node[string]{Data: "C"}
-	//t.Root.Left.Right.Right = &Node[string]{Data: "E"}
-	//
-	//t.Root.Right = &Node[string]{Data: "G"}
-	//t.Root.Right.Right = &Node[string]{Data: "I"}
-	//t.Root.Right.Right.Left = &Node[string]{Data: "H"}
-	//
-	//fmt.Println(t.Root.Left.Data, "<--", t.Root.Data, "-->", t.Root.Right.Data)
-	//fmt.Println("--------------------")
-	//
-	//fmt.Println(t.Root.Left.Left.Data, "<--", t.Root.Left.Data, "-->", t.Root.Left.Right.Data)
-	//
-	//fmt.Println("--------------------")
-	//fmt.Println(t.Root.Right.Left, "<--", t.Root.Right.Data, "-->", t.Root.Right.Right.Data)
-	//
-	//fmt.Println("--------------------")
-	//fmt.Println(t.Root.Right.Right.Left.Data, "<--", t.Root.Right.Right.Data, "-->", t.Root.Right.Right.Right)
-
-	// LEFT
-	// VISIT
-	// RIGHT
-
-}
